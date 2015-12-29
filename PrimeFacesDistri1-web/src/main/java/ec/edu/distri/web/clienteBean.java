d90fe72b71fa9569ce74347fe0142762901820b4
Dalia <Dalia@Dalia-PC>
2015-12-28 23:56:31 -0500

    faces

---
+++ PrimeFacesDistri1-web/src/main/java/ec/edu/distri/web/clienteBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.edu.distri.web;
+
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Dalia
+ */
+@ViewScoped
+@ManagedBean
+public class clienteBean implements Serializable{
+   
+    
+}
